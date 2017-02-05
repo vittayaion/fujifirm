2274cc34f00b315e0c013b3226b40966046b7dca
vittayaion <vittaya_jame@hotmail.com>
2017-02-05 22:03:32 +0700

    update1

---
+++ README.md
@@ -0,0 +1,58 @@
+# FujifilmX-T20-Landingpage
+
+### แบบทดสอบ
+สร้าง Landingpage โปรโมท กล้องตัวใหม่ของทาง FujiFilm "X-T20" 
+##### รายละเอียด เงือนไข
+- ใช้ข้อมูลที่เตรียมมาให้ข้าง[ล่างนี้](#x-t20-infomation) หรือจะเพิ่มข้อมูลก็ได้
+- รูปและภาพประกอบสามารถหามาตกแต่งได้ตามต้องการ  (รูปในเนื้อหาอยู่ [Link นี้])
+- มีฟอร์มกรอกข้อมูลเพื่อรับสาร โดยกรอก ชื่อ , email, เบอร ์โทร
+- หลังจากกรอกรับข้อมูลข่าวสารแล้ว ระบบจะมีการส่งmailให้การขอบคุณสำหรับติดตามข่าวสาร ตาม [ข้อมูลข้างล่างนี้](#mail-info)
+##### หมายเหตุ
+- Clone repository นี้ แล้วลงในgithub ของตัวเองเลยครับ
+- แล้วส่ง link github (ข้อ1 ขอตัวเอง) มาที่ mail:: kriangsak.loisaeng@gmail.com
+- คำแนะนำ:: ควรอัพเดทโค้ดตัวเองขึ้น github อย่างเสมอ
+
+# X-T20 infomation
+video link => https://youtu.be/2g9nxIYZfBA
+
+- X-Trans™* CMOS III sensor & X-Processor Pro
+- Touchscreen Operation
+- AF-C Custom Setting
+- 4K Video Recording
+- Advanced SR AUTO mode
+
+Sensor => APS-C"X-Trans CMOS III"
+Effective Pixels => 24M
+LCD Monitor => 3.0-inch tilting LCD screen (1,040K-dot)
+Sensitivity => ISO 51200**
+
+### Quality on Every Shot
+Turn the dials. Look through the viewfinder. Shoot in stunning colors. Capture the moment... The FUJIFILM X-T20 packs the latest technology into its beautifully designed compact and lightweight body so you get the most from your photography whatever you choose to shoot.
+
+### Control Mode & Auto Mode
+Capture images exactly as you see them with　dial-based controls and a variety of exposure modes, or select Advanced SR AUTO for intelligent, fully-automatic shooting.
+
+### Electronic Viewfinder & Tilting Touch Monitor
+With a magnification of 0.62x*** and a display lag time of just 0.005sec, the FUJIFILM X-T20 has a large, fast viewfinder. It also features a 3.0-inch 1.04M-dot tilting TFT color LCD monitor suitable for both above head and close to the ground shooting. By combining the Touch Shot function with the tilt LCD monitor, you can get even more creative.
+
+### Sensor & Processor
+Outstanding photo quality is achieved by the X Series' highest image resolution, the high-speed X-Processor Pro image processing engine and Fujifilm's color reproduction technology, perfected through over 80 years experience in photographic film development
+
+### Autofocus
+The number of focusing points has been dramatically expanded from 49 in the previous model to 91 (up to 325 points). Approx. 40% of the imaging area (the center area containing 49 focusing points) is covered with phase detection AF pixels to form a fast and precise phase detection AF area that can be used in a variety of scenes.
+
+### Various Functions
+The FUJIFILM X-T20 is capable of recording both Full HD and 4K video using the X Series'famous Film Simulation effects. You can output recorded video to an external monitor via the HDMI port and input audio from an external microphone.
+
+### Notes
+- \* X-Trans is a trademark or registered trademark of FUJIFILM Corporation.
+- ** Extended output sensitivity.
+- *** Viewfinder magnification ratio. Approx. 0.62x magnification 50mm (35mm format equivalent) at infinity and diopter set to -1.0 m-1.
+
+# mail-info
+* Thank you for you subscription
+* Please confirm your subscription
+
+# The End :) Have A Nice Day krub
+
+   [Link นี้]: <http://www.fujifilm.com/products/digital_cameras/x/fujifilm_x_t20/>
---
+++ index.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+  </head>
+  <body>
+      <nav class="navbar navbar-inverse bg-primary">
+            <h1 class="navbar-brand mb-0">FujifilmX-T20</h1>
+      </nav>
+
+
+    <div class="container">
+          <div class="row">
+          <img src="pic/pic_header_01.png" alt="pic1" class="pull-left">
+          <h1>X-T20 infomation</h1>
+          </div>
+          <div class="row">
+          <iframe width="600" height="415" src="https://www.youtube.com/embed/2g9nxIYZfBA?autoplay=1"></iframe>
+
+          <ul>
+            <li>X-Trans™* CMOS III sensor & X-Processor Pro</li>
+            <li>Touchscreen Operation</li>
+            <li>AF-C Custom Setting</li>
+            <li>4K Video Recording</li>
+            <li>Advanced SR AUTO mode</li>
+          </ul>
+          </div>
+    </div>
+          <div class="text-center">
+          <img src="pic/img_main01.jpg" class="img-fluid" alt="Responsive image">
+          <img src="pic/img_main02.jpg" class="img-fluid" alt="Responsive image">
+          </div>
+
+
+          <p class="text-center">Sensor => APS-C"X-Trans CMOS III" Effective Pixels => 24M LCD Monitor => 3.0-inch tilting LCD screen (1,040K-dot) Sensitivity => ISO 51200**</p>
+<section id="first">
+      <div class="container">
+          <div class="row">
+              <div class="col-sm-6">
+                  <div class="card">
+                      <div class="card-block">
+                        <div class="text-center">
+                        <img class="card-img-top img-fluid" src="pic/pic_01.jpg" alt="Card image cap"></div>
+                            <div class="card-block">
+                                <h4 class="card-title">Quality on Every Shot</h4>
+                                <p class="card-text">Turn the dials. Look through the viewfinder. Shoot in stunning colors. Capture the moment... The FUJIFILM X-T20 packs the latest technology into its beautifully designed compact and lightweight body so you get the most from your photography whatever you choose to shoot.</p>
+
+                            </div>
+
+                      </div>
+                </div>
+            </div>
+
+            <div class="col-sm-6">
+                <div class="card">
+                    <div class="card-block">
+                      <div class="text-center">
+                      <img class="card-img-top img-fluid" src="pic/pic_05.jpg" alt="Card image cap"></div>
+                          <div class="card-block">
+                              <h4 class="card-title">Auto focus</h4>
+                              <p class="card-text">The number of focusing points has been dramatically expanded from 49 in the previous model to 91 (up to 325 points). Approx. 40% of the imaging area (the center area containing 49 focusing points) is covered with phase detection AF pixels to form a fast and precise phase detection AF area that can be used in a variety of scenes.</p>
+                          </div>
+
+                    </div>
+              </div>
+          </div>
+        </div>
+
+
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="card">
+                    <div class="card-block">
+                      <div class="text-center">
+                      <img class="card-img-top img-fluid" src="pic/pic_02.jpg" alt="Card image cap"></div>
+                          <div class="card-block">
+                              <h4 class="card-title">Control Mode & Auto Mode</h4>
+                              <p class="card-text">Capture images exactly as you see them with　dial-based controls and a variety of exposure modes, or select Advanced SR AUTO for intelligent, fully-automatic shooting.</p>
+                          <br>
+                          </div>
+
+                    </div>
+              </div>
+          </div>
+
+          <div class="col-sm-6">
+              <div class="card">
+                  <div class="card-block">
+                    <div class="text-center">
+                    <img class="card-img-top img-fluid" src="pic/pic_03.jpg" alt="Card image cap"></div>
+                        <div class="card-block">
+                            <h4 class="card-title">Electronic Viewfinder & Tilting Touch Monitor</h4>
+                            <p class="card-text">With a magnification of 0.62x*** and a display lag time of just 0.005sec, the FUJIFILM X-T20 has a large, fast viewfinder. It also features a 3.0-inch 1.04M-dot tilting TFT color LCD monitor suitable for both above head and close to the ground shooting. By combining the Touch Shot function with the tilt LCD monitor, you can get even more creative.</p>
+                        </div>
+
+                  </div>
+            </div>
+        </div>
+      </div>
+
+
+      <div class="row">
+          <div class="col-sm-6">
+              <div class="card">
+                  <div class="card-block">
+                    <div class="text-center">
+                    <img class="card-img-top img-fluid" src="pic/pic_04.jpg" alt="Card image cap"></div>
+                        <div class="card-block">
+                            <h4 class="card-title">Sensor & Processor</h4>
+                            <p class="card-text">Outstanding photo quality is achieved by the X Series' highest image resolution, the high-speed X-Processor Pro image processing engine and Fujifilm's color reproduction technology, perfected through over 80 years experience in photographic film development.</p>
+                        <br>
+                        </div>
+
+                  </div>
+            </div>
+        </div>
+
+        <div class="col-sm-6">
+            <div class="card">
+                <div class="card-block">
+                  <div class="text-center">
+                  <img class="card-img-top img-fluid" src="pic/pic_06.jpg" alt="Card image cap"></div>
+                      <div class="card-block">
+                          <h4 class="card-title">Various Functions</h4>
+                          <p class="card-text">The FUJIFILM X-T20 is capable of recording both Full HD and 4K video using the X Series'famous Film Simulation effects. You can output recorded video to an external monitor via the HDMI port and input audio from an external microphone.</p>
+                          <br>
+                      </div>
+
+                </div>
+          </div>
+      </div>
+    </div>
+
+        <div class="container">
+          <h2>Notes</h2>
+          <ul>
+            <li>* X-Trans is a trademark or registered trademark of FUJIFILM Corporation.</li>
+            <li>** Extended output sensitivity.</li>
+            <li>*** Viewfinder magnification ratio. Approx. 0.62x magnification 50mm (35mm format equivalent) at infinity and diopter set to -1.0 m-1.</li>
+          </ul>
+        </div>
+
+    </div>
+</section>
+    <section id="final">
+    <div class="container" id="final">
+      <h4>Subscribe to Newsletter</h4>
+      <br>
+    <form class="form-inline">
+          <div class="form-group"><label for="exampleInputEmail1"><B>Email address : </B></label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+          </div>
+          <div class="form-group"><label for="nameInput"><B>Name :</B> </label>
+                <input type="name" class="form-control" id="InputName" placeholder="Enter your name">
+          </div>
+          <div class="form-group"><label for="example-tel-input"><B>Telephone : </B></label>
+                <input type="tel" class="form-control" value="1-(555)-555-5555" id="tel-input">
+          </div>
+          <div class="form-group">
+                <div class="col-lg-offset-2 col-lg-10">
+                <button class="btn btn-primary" type="submit" id='submit'>Sent</button>
+                </div>
+          </div>
+    </form>
+    </div>
+  </section>
+
+
+    <!-- jQuery first, then Tether, then Bootstrap JS. -->
+    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+  </body>
+</html>
